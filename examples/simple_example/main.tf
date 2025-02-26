91077bd00861fbff6b70ee94d02f814da59a678b
rahul yadav <irahul@google.com>
2025-02-27 01:37:10 +0530

    enable backup test in CI

--- examples/simple_example/main.tf
+++ examples/simple_example/main.tf
@@ -36,7 +36,7 @@ module "cloud_spanner" {
       ]
       deletion_protection = false
       database_iam        = []
-      enable_backup       = false
+      enable_backup       = true
       backup_retention    = 86400
       create_db           = true
     }
